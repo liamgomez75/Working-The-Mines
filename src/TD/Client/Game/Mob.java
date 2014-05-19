0b17a1ea3aaf68a936abeef57cfe3e9834134263
R <R@Tron>
2014-05-18 17:30:42 -0700

    *Fixed Path Finding

--- src/TD/Client/Game/Mob.java
+++ src/TD/Client/Game/Mob.java
@@ -12,13 +12,10 @@ public class Mob extends Rectangle {
     public int mobSize = 64;
     public int mobWalk = 0;
     public int upward = 0, downward = 1, right = 2, left = 3;
+    public int walkFrame = 0, walkSpeed = 40;
     public int direction = right;
     public int mobID = Value.mobAir;
     public boolean inGame = false;
-    public boolean hasUpward = false;
-    public boolean hasDownward = false;
-    public boolean hasLeft = false;
-    public boolean hasRight = false;
 
     public Mob() {
     }
@@ -27,49 +24,33 @@ public class Mob extends Rectangle {
         for (int y = 0; y < Canvas.room.block.length; y++) {
             if (Canvas.room.block[y][0].groundID == Value.groundRoad) {
                 setBounds(Canvas.room.block[y][0].x, Canvas.room.block[y][0].y, mobSize, mobSize);
-                xCoord = -1;
+                xCoord = 0;
                 yCoord = y;
             }
         }
         this.mobID = mobID;
         inGame = true;
     }
-    public int walkFrame = 0, walkSpeed = 40;
 
-    public void physic() {
+    public void physics() {
         if (walkFrame >= walkSpeed) {
             move();
             fullTileCheck();
-            changeDirection();
-            walkFrame = 0; 
-        } else {
-                walkFrame += 1;
-        }
 
-    }
-
-    
-        
-
-    private void fullTileCheck() {
-        if (mobWalk == Canvas.room.blockSize) {
-            if (direction == right) {
-                xCoord += 1;
-                hasRight = true;
-
-            } else if (direction == upward) {
-                yCoord -= 1;
-                hasUpward = true;
-            } else if (direction == downward) {
-                yCoord += 1;
-                hasDownward = true;
-            } else if (direction == left) {
-                xCoord -= 1;
-                hasLeft = true;
+            final int oppositeDirection = getOppositeDirection(direction);
+            for (int turnsMade = 0; turnsMade < 4; turnsMade++) {
+                if (isDirectionClear() && ((turnsMade == 0) || (direction != oppositeDirection))) {
+                    break;
+                } else {
+                    turnClockwise();
+                }   
             }
-            mobWalk = 0;
+            
+
+            walkFrame = 0;
+        } else {
+            walkFrame += 1;
         }
-        
     }
 
     private void move() {
@@ -85,48 +66,69 @@ public class Mob extends Rectangle {
         mobWalk += 1;
     }
 
-    private void changeDirection() {
-        if (!hasDownward) { 
-            try {
-                if (Canvas.room.block[yCoord + 1][xCoord].groundID == Value.groundRoad) {
-                    direction = downward;
-                }
-            } catch (Exception e) {}
+    private void fullTileCheck() {
+        if (mobWalk == Canvas.room.blockSize) {
+            if (direction == right) {
+                xCoord += 1;
+            } else if (direction == upward) {
+                yCoord -= 1;
+            } else if (direction == downward) {
+                yCoord += 1;
+            } else if (direction == left) {
+                xCoord -= 1;
+            }
+            mobWalk = 0;
         }
-
-
-        if (!hasUpward) {
-            try {
-                if (Canvas.room.block[yCoord - 1][xCoord].groundID == Value.groundRoad) {
-                    direction = upward;
-                }
-            } catch (Exception e) {}
-        }
-        if (!hasRight) {
-
-            try {
-                if (Canvas.room.block[yCoord][xCoord + 1].groundID == Value.groundRoad) {
-                    direction = right;
-                }
-            } catch (Exception e) {}
-        }
-
-        if (!hasLeft) {
-
-            try {
-                if (Canvas.room.block[yCoord][xCoord - 1].groundID == Value.groundRoad) {
-                    direction = left;
-                }
-            } catch (Exception e) {}
-        }
-        hasUpward = false;
-        hasDownward = false;
-        hasLeft = false;
-        hasRight = false;
     }
 
+    private int getOppositeDirection(int direction) {
+        if (direction == upward) {
+            return downward;
+        } else if (direction == downward) {
+            return upward;
+        } else if (direction == left) {
+            return right;
+        } else if (direction == right) {
+            return left;
+        } else {
+            return 4;
+        }
+    }
 
-public void draw(Graphics g) {
-        g.drawImage(Canvas.tileset_mob[mobID],x ,y ,width ,height , null);
+    private boolean isDirectionClear() {
+        if (direction == downward) {
+            if (Canvas.room.block[yCoord + 1][xCoord].groundID != Value.groundRoad) {
+                return false;
+            }
+        } else if (direction == upward) {
+            if (Canvas.room.block[yCoord - 1][xCoord].groundID != Value.groundRoad) {
+                return false;
+            }
+        } else if (direction == right) {
+            if (Canvas.room.block[yCoord][xCoord + 1].groundID != Value.groundRoad) {
+                return false;
+            }
+        } else if (direction == left) {
+            if (Canvas.room.block[yCoord][xCoord - 1].groundID != Value.groundRoad) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void turnClockwise() {
+        if (direction == upward) {
+            direction = right;
+        } else if (direction == right) {
+            direction = downward;
+        } else if (direction == downward) {
+            direction = left;
+        } else if (direction == left) {
+            direction = upward;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(Canvas.tileset_mob[mobID], x, y, width, height, null);
     }
 }
