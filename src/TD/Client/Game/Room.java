3adfb944e03a7ac2afddc72a1de48cb8736c8ef6
R <R@Tron>
2014-05-25 00:41:20 -0700

    -Added graphics for tower combat

--- src/TD/Client/Game/Room.java
+++ src/TD/Client/Game/Room.java
@@ -29,13 +29,23 @@ public class Room {
     }
     
     public void physics() {
-        
+        for(int y = 0;y < block.length;y++) {
+            for(int x = 0; x < block[0].length;x++) {
+                block[y][x].physics();
+            }
+        }
     }
     
     public void draw(Graphics g) {
         for(int y = 0; y < block.length; y++) {
             for(int x =0; x < block[0].length; x++) {
                 block[y][x].draw(g);
+            }
+        }
+        
+        for(int y = 0; y < block.length; y++) {
+            for(int x =0; x < block[0].length; x++) {
+                block[y][x].combatGraphics(g);
             }
         }
     }
